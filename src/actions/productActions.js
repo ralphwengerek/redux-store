e5a6d70df4054707998fcb295c2f7d4039a99358
Ralph Wengerek <ralph.wengerek@gmail.com>
2019-10-01 10:00:49 +0100

    update packages

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,7 +1,21 @@
-const fetchProducts = () => {
-  return {
-    type: "FETCH_PRODUCTS"
-  };
-};
 
-export default fetchProducts;
+
+const fetchProducts = () => ({
+    type: "FETCH_PRODUCTS",
+  });
+
+const fetchProduct = (id) => ({
+    type: "FETCH_PRODUCTS",
+    id,
+  });
+
+const addProduct = (product) => ({
+  type: 'ADD_PRODUCT',
+  payload: product,
+});
+
+export {
+  addProduct,
+  fetchProducts,
+  fetchProduct,
+};
--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,9 +1,11 @@
 const productReducer = (state = [], action) => {
   switch (action.type) {
+    case "FETCH_PRODUCTS":
+      return [...state];
     case "FETCH_PRODUCT":
-      return state;
+      return state.find(item => item.id === action.id);
     case "ADD_PRODUCT":
-      return state;
+      return [...state, action.payload];
     default:
       return state;
   }
